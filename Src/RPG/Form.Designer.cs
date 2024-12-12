99d33ab60a1a4e010bfcb6ae56b62a5cf3faeebe
Pedro Ferraz <pepito.ferraz@gmail.com>
2024-12-12 17:57:11 -0300

    background home page

--- Src/RPG/Form.Designer.cs
+++ Src/RPG/Form.Designer.cs
@@ -20,12 +20,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRPG));
             this.btnjogar = new System.Windows.Forms.Button();
             this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnjogar
             // 
             this.btnjogar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnjogar.Location = new System.Drawing.Point(210, 124);
+            this.btnjogar.Location = new System.Drawing.Point(208, 192);
             this.btnjogar.Name = "btnjogar";
             this.btnjogar.Size = new System.Drawing.Size(135, 26);
             this.btnjogar.TabIndex = 0;
@@ -37,7 +38,7 @@
             // btnSair
             // 
             this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSair.Location = new System.Drawing.Point(239, 165);
+            this.btnSair.Location = new System.Drawing.Point(235, 224);
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(75, 23);
             this.btnSair.TabIndex = 1;
@@ -45,13 +46,26 @@
             this.btnSair.UseVisualStyleBackColor = true;
             this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(122, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "A Jornada Do Herói";
+            // 
             // frmRPG
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(544, 371);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnSair);
             this.Controls.Add(this.btnjogar);
             this.MaximizeBox = false;
@@ -62,6 +76,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RPG";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +84,7 @@
 
         private System.Windows.Forms.Button btnjogar;
         private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
     }
 }
 
