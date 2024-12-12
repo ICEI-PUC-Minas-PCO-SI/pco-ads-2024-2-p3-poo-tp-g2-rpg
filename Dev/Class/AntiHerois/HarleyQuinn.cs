32311a6aa2fa5d14ecbdef89fbdf9a360aa3b8a4
Pedro Ferraz <pepito.ferraz@gmail.com>
2024-12-12 00:35:45 -0300

    Adição de personagens e algumas logicas do jogo

---
+++ Dev/Class/AntiHerois/HarleyQuinn.cs
@@ -0,0 +1,27 @@
+﻿
+using RPG.Class.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class HarleyQuinn : AntiHeroi
+{
+    public HarleyQuinn()
+    {
+        this.Nome = "HarleyQuinn";
+        this.PontosdeVida = 3000;
+        this.PontosdeMana = 40;
+        this.ForcaFisica = 100;
+        this.ForcaMagica = 20;
+        this.PontoAmrmadura = 100;
+        this.ResistenciaMagica = 50;
+        this.Agilidade = 80;
+        this.NomeImegem = "FtoDragao.jpg";
+        VetItem[0] = new HalitoDeFogo();
+        VetItem[1] = new GarraLetal();
+
+        this.MenorArma();
+    }
+}
+
---
+++ Dev/Class/Heroi/Ben10.cs
@@ -0,0 +1,31 @@
+﻿
+using RPG.Class.Item;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Ben10 : Heroi
+{
+    public Ben10()
+    {
+        this.Nome = "Ben10";
+        this.PontosdeVida = 4000;
+        this.PontosdeMana = 30;
+        this.ForcaFisica = 100;
+        this.ForcaMagica = 20;
+        this.PontoAmrmadura = 80;
+        this.ResistenciaMagica = 20;
+        this.Agilidade = 50;
+        this.NomeImegem = "FtoGuerreiro.jpg";
+
+
+
+
+        VetItem[0] = new Porrete();
+        VetItem[1] = new EspadaBarroca();
+        VetItem[2] = new Tempestade();//Olhar nas especificações
+        this.MenorArma();
+    }
+}
+
